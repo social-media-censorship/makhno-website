6c7aec5dcac680af19e6646ec619bb296b05fc61
Anxhelo Lushka <anxhelo1995@gmail.com>
2023-05-30 12:57:46 +0200

    Added a part of the results page

--- assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-var heading = document.getElementById("styled-title");
-var words = heading.textContent.split(" ");
-words[0] = "<span id='first-word'>" + words[0] + "</span>";
-words[words.length - 1] =
-  "<span id='last-word'>" + words[words.length - 1] + "</span>";
-heading.innerHTML = words.join(" ");
